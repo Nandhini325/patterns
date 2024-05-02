16f49e300e791097b26f60395466d54bfc00dbfd
Nandhini325 <nandhinirajendran33@gmail.com>
2024-05-02 20:19:41 +0530

    first commit

---
+++ pyramid.java
@@ -0,0 +1,20 @@
+class lefthalfpyramid{
+    public static void main(String[] args) {
+     for (int i = 1; i <=5; i++){
+        for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+        }
+            System.out.println();
+        }
+    }
+}
+class invertedlefhalfpyramid{
+    public static void main(String args[]){
+        for(int i=1; i<=5;i++) {
+            for(int j =5;j>=i;j--){
+                System.out.print("*");}
+                System.out.println();
+
+            }
+        
+        }    } 
