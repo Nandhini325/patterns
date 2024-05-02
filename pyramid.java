442d487b755ab06bc327f3148a62d15cee173aaf
Nandhini325 <nandhinirajendran33@gmail.com>
2024-05-02 20:35:13 +0530

    trial

--- pyramid.java
+++ pyramid.java
@@ -1,20 +1,23 @@
-class lefthalfpyramid{
+class lefthalfpyramid {
     public static void main(String[] args) {
-     for (int i = 1; i <=5; i++){
-        for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("* ");
-        }
+            }
             System.out.println();
         }
     }
 }
-class invertedlefhalfpyramid{
-    public static void main(String args[]){
-        for(int i=1; i<=5;i++) {
-            for(int j =5;j>=i;j--){
-                System.out.print("*");}
-                System.out.println();
 
+class invertedlefhalfpyramid {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print("*");
             }
-        
-        }    } 
+            System.out.println();
+
+        }
+
+    }
+}
