dd444b860a93a1c01941397f5b45c103d0d81c13
Nandhini325 <nandhinirajendran33@gmail.com>
2024-05-05 14:34:54 +0530

    pyramids

--- pyramid2.java
+++ pyramid2.java
@@ -13,8 +13,8 @@ class lefthalfpyramid {
 }
 
 class invertedlefthalfpyramid {
- public static void main(String[] args){
-        for (int i =5; i>= 1; i--) {
+    public static void main(String[] args) {
+        for (int i = 5; i >= 1; i--) {
             for (int j = 5; j > i; j--) {
                 System.out.print(" ");
             }
