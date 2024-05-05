efa212cac905e74e8e5425ee989c7d9e45f8d6d1
Nandhini325 <nandhinirajendran33@gmail.com>
2024-05-05 14:37:55 +0530

    4pyramids

--- pyramid2.java
+++ pyramid2.java
@@ -25,3 +25,27 @@ class invertedlefthalfpyramid {
         }
     }
 }
+
+class righthalfpyramid {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+class invertedrighthalfpyramid {
+    public static void main(String args[]) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = 5; j >= i; j--) {
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+
+    }
+}
